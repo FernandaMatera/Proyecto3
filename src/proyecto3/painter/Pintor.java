5fe7ba3fab880e64f589ff30fbb7119a667f36a6
Luxho97 <l.seba.aburto@gmail.com>
2016-12-10 19:50:33 -0300

    Aqui va el Sr Pintor
    Si falla quiero que sepas que es tu culpa y te odio, si funciona.. te
    odio de igual manera

--- src/proyecto3/painter/Pintor.java
+++ src/proyecto3/painter/Pintor.java
@@ -5,10 +5,67 @@
  */
 package proyecto3.painter;
 
-/**
- *
- * @author Usuario
- */
-public class Pintor {
+import java.util.Iterator;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import proyecto3.Sistema;
+import proyecto3.Caramelo;
+import proyecto3.Dimension;
+
+
+import proyecto3.Sistema;
+
+public class Pintor 
+{
+    static public void pintar( Sistema juego, GraphicsContext context, Dimension mundo, Dimension ventana )
+    {
+  
+        Pintor.dibujar( context, mundo, ventana);
+            
+
+        
+        Iterator<Caramelo> caramelos = juego.iterator();
+        while( caramelos.hasNext() )
+        {
+            Caramelo caramelo = caramelos.next();
+            Pintor.dibujar(caramelo, context, mundo, ventana);
+        }
+        
+    }
+    
+ 
+   
+    
+    static private void dibujar(Caramelo caramelo, GraphicsContext context, Dimension mundo, Dimension ventana)
+    {
+        int x = Pintor.convertirXACoordenadasVentana(caramelo.getX(), mundo, ventana);
+        int y = Pintor.convertirYACoordenadasVentana(caramelo.getY(), mundo, ventana);
+        int ancho = Pintor.convertirXACoordenadasVentana(caramelo.getAncho(), mundo, ventana);
+        int alto = Pintor.convertirYACoordenadasVentana(caramelo.getAlto(), mundo, ventana);
+        context.drawImage(CargadorImagenes.getImage( caramelo.getCaramelo().getFilename()), x, y, ancho, alto);
+    }
+    
+    
+    static private int convertirXACoordenadasVentana(int x, Dimension mundo, Dimension ventana)
+    {
+        return x*ventana.getWidth()/mundo.getWidth();
+    }
+    
+    static private int convertirYACoordenadasVentana(int y, Dimension mundo, Dimension ventana)
+    {
+        return y*ventana.getHeight()/mundo.getHeight();
+    }
+    
+    static private int convertirMagnitudACoordenadasVentana(int magnitud, Dimension mundo, Dimension ventana)
+    {
+        double diagonalMundo = Math.sqrt( mundo.getWidth()*mundo.getWidth() + mundo.getHeight()*mundo.getHeight() );
+        double diagonalVentana = Math.sqrt( ventana.getWidth()*ventana.getWidth() + ventana.getHeight()*ventana.getHeight() );
+        return (int)(magnitud*diagonalVentana/diagonalMundo);
+    }
+
+    private static void dibujar(GraphicsContext context, Dimension mundo, Dimension ventana) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
+
