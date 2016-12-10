18f1bad1d1611a97d9ba18ebb12d63ee0f301cb7
Luxho97 <l.seba.aburto@gmail.com>
2016-12-10 10:53:39 -0300

    Pero khe verguitas
    Esperemos esto resulte

--- src/proyecto3/painter/CargadorImagenes.java
+++ src/proyecto3/painter/CargadorImagenes.java
@@ -3,12 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto3;
+package proyecto3.painter;
+
+import java.util.HashMap;
+import javafx.scene.image.Image;
 
 /**
  *
- * @author Usuario
+ * @author Luis
  */
-public class CargadorImagenes {
+public class CargadorImagenes 
+{
+    static private final HashMap<String, Image> IMAGENES = new HashMap<>();
     
+    static public Image getImage(String filename)
+    {
+        Image image = CargadorImagenes.IMAGENES.get(filename);
+        if( image != null )
+        {
+            return image;
+        }
+        
+        image = new Image(Pintador.class.getResourceAsStream("/proyecto3/images/" + filename));
+        CargadorImagenes.IMAGENES.put(filename, image);
+        return image;
+    }
 }
